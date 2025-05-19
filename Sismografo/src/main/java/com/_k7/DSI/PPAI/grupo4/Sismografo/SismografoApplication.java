ad421613f48cde73ab14cad0eb5846a065651f4a
PardoJuan <snowshuriken@gmail.com>
2025-05-19 13:42:35 -0300

    spring.io

---
+++ Sismografo/src/main/java/com/_k7/DSI/PPAI/grupo4/Sismografo/SismografoApplication.java
@@ -0,0 +1,13 @@
+package com._k7.DSI.PPAI.grupo4.Sismografo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismografoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismografoApplication.class, args);
+	}
+
+}
---
+++ Sismografo/src/test/java/com/_k7/DSI/PPAI/grupo4/Sismografo/SismografoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._k7.DSI.PPAI.grupo4.Sismografo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SismografoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
